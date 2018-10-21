a80b55910cf73c9f819794b8cd2334a827df7873
Cova8bitdot <cova.tech.8bitdots@gmail.com>
2018-10-21 15:49:49 +0900

    [UI] IngameScene 定義追加

--- SpzGameJam/Assets/Scripts/UI/IngameUIManager.cs
+++ SpzGameJam/Assets/Scripts/UI/IngameUIManager.cs
@@ -14,6 +14,11 @@ namespace GameJam.UI
         {
 
         }
+
+
+        public static readonly System.Type INIT_STATE                       = typeof(Ingame_Init);
+        public static readonly System.Type DEFAULT_STATE                    = typeof(Ingame_Default);
+        public static readonly System.Type RESULT_STATE                     = typeof(Ingame_Result);
         #endregion //) ===== CONSTS ===== 
 
         //-----------------------------------------
@@ -28,6 +33,11 @@ namespace GameJam.UI
         [SerializeField, Tooltip("BopUI")]
         BottomUIController m_BotUICtrl = null;
         BottomUIController BotUICtrl{get{return m_BotUICtrl;}}
+
+
+        [SerializeField, Tooltip("リザルトUI")]
+        GameObject m_ResultUIPrefab = null;
+        GameObject ResultUIPrefab{get{return m_ResultUIPrefab;}}
 
         #endregion //) ===== MEMBER_VATRIABLES ===== 
 
@@ -56,10 +66,26 @@ namespace GameJam.UI
             TopUICtrl?.Initialize( new TopUIController.InitParam() );
             BotUICtrl?.Initialize( new BottomUIController.InitParam() );
         }
-
-    
+   
 
         #endregion //) ===== INITIALIZE ===== 
 
+        //-----------------------------------------
+        // StateMachine 管理
+        //-----------------------------------------
+        #region ===== STATE_MACHINE_CONTROLL =====
+
+        /// <summary>
+        /// StateMachine 初期化メソッド
+        /// </summary>
+        protected override void InitMachine()
+        {
+            base.InitMachine();
+            SMachine.Start(INIT_STATE);
+            PushNextState(INIT_STATE);
+
+        }
+       #endregion //) ===== STATE_MACHINE_CONTROLL =====
+ 
     }
 }
---
+++ SpzGameJam/Assets/Scripts/UI/System/Editor/IngameUIManagerEditor.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using GameJam.UI;
+using UnityEditor;
+
+
+[CustomEditor(typeof(IngameUIManager))]
+public class IngameUIManagerEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+
+		IngameUIManager manager = target as IngameUIManager;
+
+		EditorGUILayout.BeginVertical(GUI.skin.box);
+		{
+			//Title
+			EditorGUILayout.LabelField("State", GUI.skin.box);
+
+			EditorGUILayout.BeginVertical(GUI.skin.box);
+			{
+				var names = manager.GetStateNameArray();
+
+				for (int i = 0; i < names.Length; i++)
+				{
+					EditorGUILayout.LabelField(
+						string.Format("[State {0}] {1}", i, names[i])
+						);
+				}
+			}
+			EditorGUILayout.EndVertical();
+		}
+		EditorGUILayout.EndVertical();
+
+		Repaint();
+    }
+}
